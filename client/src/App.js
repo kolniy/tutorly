1352f4a02d83c6b41a1310106ebbe1d5546b47e7
kolniy <kolaniyi3@gmail.com>
2021-05-07 17:06:04 +0100

    completed layout for dashboard pages

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,11 @@ import Signup from "./components/auth/Signup"
 import Signin from "./components/auth/Signin"
 import Setpageone from "./components/account-setup/Setuppageone"
 import Setpagetwo from "./components/account-setup/Setuppagetwo"
+import CreateCourse from "./components/dashboard/CreateCourse"
+import Courses from "./components/dashboard/Courses"
+import Customize from "./components/dashboard/Customize"
+import Sales from "./components/dashboard/Sales"
+import Messages from "./components/dashboard/Messages"
 
 function App() {
   return (
@@ -18,6 +23,11 @@ function App() {
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/account/setup/stepone" component={Setpageone} />
         <Route exact path="/account/setup/steptwo" component={Setpagetwo} />
+        <Route exact path="/dashboard/createcourse" component={CreateCourse} />
+        <Route exact path="/dashboard/courses" component={Courses} />
+        <Route exact path="/dashboard/customize" component={Customize} />
+        <Route exact path="/dashboard/sales" component={Sales} />
+        <Route exact path="/dashboard/messages" component={Messages} />
       </Switch>
     </Router>
   )
---
+++ client/src/components/dashboard/CreateCourse.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { 
+    Row,
+    Col,
+    Container,
+    Nav, 
+    NavItem, 
+    NavLink,
+    PopoverBody,
+    UncontrolledPopover
+} from "reactstrap"
+import dummyAvatar from "../../images/dummy-avatar.png"
+
+import "../../custom-styles/dashboard/dashboardlayout.css";
+
+const CreateCourse = () => {
+    return <>
+        <div className="dashboard-layout">
+            <Container fluid>
+                <Row>
+                    <Col sm="2" md="2" xs="2" xl="2">
+                        <div className="vertical-navbar">
+                            <div className="tutorly-logo">
+                                TUTORLY
+                            </div>
+                            <div className="logged-in-user__info">
+                                <div className="logged-in-user__image">
+                                    <img src={dummyAvatar} 
+                                    alt="user avatar"
+                                    className="img-fluid"
+                                 />
+                                </div>
+                            </div>
+                            <p className="logged-in-user__school-name d-none-sm">Eustice Jack Film School</p>
+                            <p className="logged-in-user__email d-none-sm">eusticethemenace2020@gmail.com</p>
+                            <div className="action-links">
+                                <Nav vertical>
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/dashboard/createcourse">
+                                        <i className="fas fa-plus"></i> <span className="navlink-text d-none-sm">Create New Course</span>
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/dashboard/courses">
+                                        <i className="fas fa-book"></i> <span className="navlink-text d-none-sm">My Courses</span>
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/dashboard/customize">
+                                        <i className="fas fa-edit"></i> <span className="navlink-text d-none-sm">Customize space</span>
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/dashboard/sales">
+                                        <i className="fas fa-dollar-sign"></i><span className="navlink-text d-none-sm">Sales</span>
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/dashboard/messages">
+                                        <i className="far fa-comment-alt"></i> <span className="navlink-text d-none-sm">Messages</span>
+                                        </NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </div>
+                            <div className="user-settings">
+                                <div className="setting-option">
+                                    <i className="fas fa-cog"></i> Settings
+                                </div>
+                                <div id="tooltipbutton" className="more-options">
+                                    <i className="fas fa-ellipsis-v"></i>
+                                </div>
+                                <UncontrolledPopover placement="top" target="tooltipbutton">
+                                        <PopoverBody>
+                                         <Nav vertical>
+                                         <NavItem>
+                                        <NavLink className="link-text" href="#">
+                                        <i className="fas fa-info-circle"></i> <span className="navlink-text">Help</span>
+                                        </NavLink>
+                                       </NavItem>
+                                       <NavItem>
+                                        <NavLink className="link-text" href="#">
+                                        <i className="fas fa-sign-out-alt"></i> <span className="navlink-text">Logout</span>
+                                        </NavLink>
+                                       </NavItem>
+                                         </Nav>
+                                        </PopoverBody>
+                                </UncontrolledPopover>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                       <div className="page-actions">
+                            
+                       </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    </>
+}
+
+export default CreateCourse
